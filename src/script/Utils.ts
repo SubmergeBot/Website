b97c495aeaa8ea209c907a6a3be45d7c1cfc4703
andre4ik3 <andrey@andre4ik3.email>
2021-06-10 20:00:55 +0000

    Add a custom logger

--- src/script/Utils.ts
+++ src/script/Utils.ts
@@ -1,3 +1,5 @@
+type LogMethod = "debug" | "log" | "info" | "warn" | "error";
+
 export async function fetchPage(url: string, raw = false) {
   let response: string;
 
@@ -14,7 +16,7 @@ export async function fetchPage(url: string, raw = false) {
     xhr.open("GET", url);
     xhr.send();
 
-    response = await new Promise((resolve, reject) => {
+    response = await new Promise((resolve) => {
       xhr.onload = () => resolve(xhr.responseText);
       xhr.onerror = () => resolve("");
     });
@@ -32,4 +34,29 @@ export async function sitemap() {
   const data = await fetchPage("/sitemap.txt", true);
   const sitemapRelative = data?.split("\n").map((url) => url.substring(24));
   return sitemapRelative || [];
+}
+
+export class Logger {
+  prefix: string[];
+
+  constructor(process: string, color: string) {
+    const styles = [
+      `background-color: ${color}`,
+      "border-radius: 0.5em",
+      "color: white",
+      "font-weight: bold",
+      "padding: 2px 0.5em",
+    ];
+    this.prefix = [`%c${process}`, styles.join(";")];
+  }
+
+  log(method: LogMethod, content: string): void {
+    console[method](...this.prefix, content);
+  }
+
+  group(label: string, method: LogMethod, content: string[]): void {
+    console.groupCollapsed(...this.prefix, label);
+    content.forEach((value) => console[method]);
+    console.groupEnd();
+  }
 }
