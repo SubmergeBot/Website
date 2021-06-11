03f0d1ce32ab9d0b3ed2e6454d65dd1a1f5020e4
andre4ik3 <andrey@andre4ik3.email>
2021-06-11 08:38:42 +0000

    Build site

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,5 @@ module.exports = {
     collect: { staticDistDir: "./dist" },
     upload: { target: "temporary-public-storage" },
     assert: { preset: "lighthouse:recommended" },
-    // ...
   },
 };
